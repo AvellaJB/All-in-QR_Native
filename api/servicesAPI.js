cba4912d62eb7eef52ee688679a46a44970a979e
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-05-13 16:49:47 +0200

    Simple scanner works with scalingo

--- api/servicesAPI.js
+++ api/servicesAPI.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 import { API_URL } from "@env";
+import env from "../env";
 
-const baseURL = API_URL;
+const baseURL = env.API_URL;
 
 const base = axios.create({ baseURL });
 
-const servicesAPI = {};
+const servicesAPI = {
+  checkEntrance(id) {
+    return base
+      .get(`/attendees/${id}`)
+      .then((res) => res.data)
+      .catch((err) => console.log("errorServicesAPI", err));
+  },
+};
 
 export default servicesAPI;
